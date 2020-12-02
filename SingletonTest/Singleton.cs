5f873c187efdea7bc6516d58716dad513a2feff6
Rashid Altayeb Hassan <50522480+rashidaltayeb@users.noreply.github.com>
2020-12-02 18:19:11 +0200

    Update Singleton.cs

--- SingletonTest/Singleton.cs
+++ SingletonTest/Singleton.cs
@@ -2,10 +2,11 @@ using System;
 
 namespace SingletonTest
 {
-    public class Singleton
+    public sealed class Singleton
     {
-        private static int _counter;
-        private static Singleton _singleton; //declare the instance of singleton class with null value  
+        private static int _counter; //count number of the all instance that was created
+        private static Singleton _singleton; //declare the instance of singleton class with null value 
+        // here check the instance is null or not
         public static Singleton GeSingleton
         {
             get
@@ -15,11 +16,13 @@ namespace SingletonTest
                 return _singleton;
             }
         }
+        // Declare a private constructor 
         private Singleton()
         {
             _counter++;
             Console.WriteLine(@"Index Of Object ={0}",_counter.ToString());
         }
+        // PrintMessage is a method jsut return string value
         public void PrintMessage(string msg)
         {
             Console.WriteLine(msg);
